f69c74d24c92a10d8b3f5a57e2248983d64af2cf
Gerard Ramos <gerard.ramosm89@gmail.com>
2017-09-20 16:59:47 -0700

    adding spinner

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
 import { View, Text, TextInput } from 'react-native';
 import { Button, Card, CardSection, Input } from './common';
+import firebase from 'firebase';
 
 export default class LoginForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
       email: '',
-      password: ''
+      password: '',
+      error: ''
     }
     this.styles = {
       inputStyle: {
         height: 20,
         width: 100
+      },
+      errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
       }
     }
+  }
+  onButtonPress() {
+    const { email, password } = this.state;
+    this.setState({ error: '' })
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .catch((err) => {
+            this.setState({ error: `Authentication failed ${err}` })
+          });
+      });
   }
   render() {
     return (
@@ -42,8 +60,11 @@ export default class LoginForm extends Component {
             }}
             />
           </CardSection>
+          <Text style={this.styles.errorTextStyle}>
+            {this.state.error}
+          </Text>
           <CardSection>
-            <Button>
+            <Button onPress={this.onButtonPress.bind(this)}>
               Log in
             </Button>
           </CardSection>
